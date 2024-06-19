b37424f5923e5984b980924beffb740129123c25
scnkera <149810869+scnkera@users.noreply.github.com>
2024-06-18 21:42:51 -0400

    Updated html, css, and script

--- script.js
+++ script.js
@@ -35,17 +35,28 @@ console.log("JS Linked");
 // 	};
 // }
 
+let xPlayersTurn = true;
+
 const buttonClicked = (event) => {
 	if (event.target.innerHTML !== "&nbsp;") {
 		alert("You little cheater");
 	} else {
-		const rowIndex = event.target.getAttribute("data-rowindex");
-		const colIndex = event.target.getAttribute("data-colindex");
+		const rowIndex = event.target.getAttribute("data-rowIndex");
+		const colIndex = event.target.getAttribute("data-colIndex");
 
 		board[rowIndex][colIndex] = xPlayersTurn ? "X" : "O";
 		xPlayersTurn = !xPlayersTurn;
 
 		generateBoard();
+
+		const winner = checkWin(board);
+		if (winner) {
+            setTimeout(()=>{
+                alert(winner + " has won");
+                resetBoard();
+            }, 250);
+
+		}
 	}
 };
 
@@ -68,3 +79,52 @@ const generateBoard = () => {
 };
 
 generateBoard();
+
+const checkWin = (board) => {
+	// Check rows
+	for (let i = 0; i < 3; i++) {
+		if (
+			board[i][0] &&
+			board[i][0] === board[i][1] &&
+			board[i][0] === board[i][2]
+		) {
+			return board[i][0]; // Return the winning player ("X" or "O")
+		}
+	}
+
+	// Check columns
+	for (let i = 0; i < 3; i++) {
+		if (
+			board[0][i] &&
+			board[0][i] === board[1][i] &&
+			board[0][i] === board[2][i]
+		) {
+			return board[0][i]; // Return the winning player ("X" or "O")
+		}
+	}
+
+	// Check diagonals
+	if (
+		board[0][0] &&
+		board[0][0] === board[1][1] &&
+		board[0][0] === board[2][2]
+	) {
+		return board[0][0]; // Return the winning player ("X" or "O")
+	}
+	if (
+		board[0][2] &&
+		board[0][2] === board[1][1] &&
+		board[0][2] === board[2][0]
+	) {
+		return board[0][2]; // Return the winning player ("X" or "O")
+	}
+
+	// No winner
+	return null;
+};
+
+let resetBoard = [
+	["", "", ""],
+	["", "", ""],
+	["", "", ""],
+];
